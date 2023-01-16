4be26e1b39411476f9c53e90d7e3206023717a81
Dan Philps <55665698+danphilps@users.noreply.github.com>
2023-01-16 15:25:23 +0000

    Update InvestmentToolkit.py

--- InvestmentToolkit.py
+++ InvestmentToolkit.py
@@ -1630,7 +1630,7 @@ class SAIInvesting():
 
         # Train SAI...
         # ==========================
-        sai_mod = SAIInvesting.SAI(params=params)
+        sai_mod = SAI(params=params)
         sai_mod.fit(X=sai_train_X, y=sai_train_y)
 
         # Generate test data
